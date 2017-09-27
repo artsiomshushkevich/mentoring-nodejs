2a6716c2d3c6488830449525ebb21355c10a7c03
Artsiom Shushkevich <artemshushkevich@live.com>
2017-09-27 15:19:40 +0300

    Add execution of Importer and DirWatcher functionality

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,29 @@
 
 const config = require('../config/config.json');
 import models from './models';
+import Importer from './importer';
 
+//HW1
 console.log(config.name);
 
 let user = new models.User();
 let product = new models.Product();
+
+//HW2
+let path = __dirname + '/data/';
+let delay = 5000;
+
+let importer = new Importer();
+
+importer.dirWatcher
+    .watch(path, delay)
+    .on('dirwatcher:changed', () => {
+
+  // console.log(importer.importSync(path));
+
+    importer.import(path)
+        .then((filesAsJson) => {
+            console.log(filesAsJson);
+        });
+});
+
