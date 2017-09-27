4cfc9a654c67069ede9d1ba89bb47e7231f7e4bc
Artsiom Shushkevich <artemshushkevich@live.com>
2017-09-27 11:26:17 +0300

    Implement watch and unwatch methods

--- server/src/dirwatcher.js
+++ server/src/dirwatcher.js
@@ -1,10 +1,43 @@
 'use strict';
 
-export default class DirWatcher {
-    constructor() {
-        console.log('PPP');
-    }
-    watch(path, delay) {
+import * as fs from 'fs';
+import * as pathResolver from 'path';
+import {EventEmitter} from 'events';
 
+export default class DirWatcher extends EventEmitter {
+    constructor() {
+        super();
+        this.intervalTimeout = null;
+        this.dirSize = null;
+    }
+
+    watch(path, delay) {
+        let intervalCallback = () => {
+           try {
+                let files = fs.readdirSync(path, 'utf8');
+                let newDirSize = 0;
+                
+                for (let i = 0; i < files.length; i++) {
+                    newDirSize += fs.statSync(pathResolver.join(path, files[i])).size;
+                }
+    
+                if (this.dirSize !== null && this.dirSize !== newDirSize) {
+                    this.emit('dirwatcher:changed');
+                }
+
+                console.log ('old value %d new value', this.dirSize, newDirSize);
+                this.dirSize = newDirSize;
+                
+            } catch (error) {
+                this.unwatch();
+            }
+            
+        };
+
+        this.intervalTimeout = setInterval(intervalCallback, delay, path);
+    }
+
+    unwatch() {
+        clearInterval(this.intervalTimeout);
     }
 }
