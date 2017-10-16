f39f79cd5269c9c87807ebd15a5d204c023e077e
Artsiom Shushkevich <artemshushkevich@live.com>
2017-10-17 00:14:51 +0300

    Add transform function

--- server/src/utils/streams.js
+++ server/src/utils/streams.js
@@ -13,7 +13,17 @@ function inputOutput(filePath) {
     reader.pipe(process.stdout);
 }
 
+function transform() {
+    let transformCallback = function(chunk, encoding, callback) {
+        let transformedChunk = chunk.map(charCode => String.fromCharCode(charCode).toUpperCase().charCodeAt(0));
+        callback(null, transformedChunk);
+    };
 
+    process.stdin.pipe(through(transformCallback)).pipe(process.stdout);
+}
+
+
+//transform();
 //inputOutput(__dirname + '/streams.js');
 
 
