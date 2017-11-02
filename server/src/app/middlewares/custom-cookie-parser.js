df47dba7b9a9643af4c3b0662289b3693d9392b2
Artsiom Shushkevich <artemshushkevich@live.com>
2017-11-02 15:18:10 +0300

    Implement custom cookie parser

--- server/src/app/middlewares/custom-cookie-parser.js
+++ server/src/app/middlewares/custom-cookie-parser.js
@@ -1,5 +1,17 @@
+function parseCookies(req) {
+    var parsedCookies = {},
+        reqCookies = req.headers.cookie;
+
+    reqCookies && reqCookies.split(';').forEach(function(cookie) {
+        var parts = cookie.split('=');
+        parsedCookies[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+    return parsedCookies;
+}
 
 export default (req, res, next) => {
-   // console.dir(req.headers.cooki)
+    req.parsedCookies = parseCookies(req);
+    console.dir(req.parsedCookies)
     next();
 };
