55aabf8d29027ed51a88c423803471c19c6aae51
Artsiom Shushkevich <artemshushkevich@live.com>
2017-11-02 10:25:14 +0300

    Add html server

---
+++ server/src/http-servers/html-server.js
@@ -0,0 +1,17 @@
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+
+const htmlServer = http.createServer();
+
+htmlServer.on('request', (req, res) => {
+    const htmlFile = fs.readFileSync(path.resolve('./src/http-servers/index.html'));
+    const htmlFileContent = htmlFile.toString().replace('{message}', 'Any message');
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end(htmlFileContent);
+});
+
+htmlServer.listen(3000);
